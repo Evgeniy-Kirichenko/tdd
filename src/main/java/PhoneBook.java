ce9f5b989d5a911a634cae44c5863c2da3f6bd81
Evgeni Kirichenko <avikon3412@yandex.ru>
2023-01-30 18:26:25 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,6 @@
 public class PhoneBook {
+    
+    public boolean add() {
+        return false;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PhoneBookTest {
+    PhoneBook phoneBook = new PhoneBook();
+
+    @Test
+    public void addTest() {
+        boolean expected = true;
+        var result = phoneBook.add();
+        assertEquals(expected,result);
+    }
 }
